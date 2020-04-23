ab8faf0eb45a54e35247db76d594979595a52de1
osama adam <osamaadam98@gmail.com>
2020-04-23 18:41:44 +0200

    addec limits

--- main.m
+++ main.m
@@ -12,19 +12,26 @@ filteredSig = lowPassFilter(audio, 4e3, Fs);
 
 audiowrite('filteredSig.wav', filteredSig, Fs);
 
-figure;subplot(2, 1, 1)
+figure;
+subplot(2, 1, 1)
 plot(t, audio);
-title('Before Filter')
+ylim([-0.5, 0.5]);
+title('Before Filter');
 subplot(2, 1, 2);
 plot(t, filteredSig);
+ylim([-0.5, 0.5]);
 title('After Filter');
 
 figure;
 subplot(2, 1, 1);
 plot(fs, real(fftshift(fft(audio))));
+xlim([-1e4, 1e4]);
+ylim([-500, 500]);
 title('Before Filter');
 subplot(2, 1, 2);
 plot(fs, real(fftshift(fft(filteredSig))));
+xlim([-0.5e4, 0.5e4]);
+ylim([-500, 500]);
 title('After Filter');
 
 Fc = 100e3;
@@ -47,6 +54,8 @@ plot(t, modulatedSCSig);
 title('SSB-SC Time Domain')
 subplot(2,1,2); 
 plot(fs, real(fftshift(fft(modulatedSCSig))));
+xlim([-1.2e5, 1.2e5]);
+ylim([-2.5e3, 2.5e3])
 title('SSB-SC Frequency Domain')
 
 % Coherent Detection
@@ -57,9 +66,12 @@ audiowrite('coherentSC.wav', coherentSC, Fs);
 figure;
 subplot(2, 1, 1);
 plot(t, coherentSC);
+ylim([-0.05, 0.05]);
 title('Suppressed Carrier Coherent');
 subplot(2, 1, 2);
 plot(fs, real(fftshift(fft(coherentSC))));
+xlim([-0.5e4, 0.5e4]);
+ylim([-1e3, 1e3]);
 title('Suppressed Carrier Coherent Spectrum');
 
 % Butterworth Coherent Detection
@@ -72,6 +84,8 @@ plot(t, butteredSig);
 title('Suppressed Carrier Coherent (Butterworth)');
 subplot(2, 1, 2);
 plot(fs, real(fftshift(fft(butteredSig))));
+xlim([-0.5e4, 0.5e4]);
+ylim([-1e3, 1e3]);
 title('Suppressed Carrier Coherent Spectrum (Butterworth)');
 
 % Bad SNR
@@ -85,17 +99,19 @@ audiowrite('coherentSC30SNR.wav', coherentSC30SNR, Fs);
 figure;
 subplot(2, 1, 1);
 plot(t, coherentSC10SNR);
-title('Suppressed Carrier Coherent 10 SNR')
+title('Suppressed Carrier Coherent 10 SNR');
 subplot(2, 1, 2);
 plot(fs, real(fftshift(fft(coherentSC10SNR))));
+xlim([-0.5e4, 0.5e4]);
 title('Suppressed Carrier Coherent Spectrum 10 SNR');
 
 figure;
 subplot(2, 1, 1);
 plot(t, coherentSC30SNR);
-title('Suppressed Carrier Coherent 30 SNR')
+title('Suppressed Carrier Coherent 30 SNR');
 subplot(2, 1, 2);
 plot(fs, real(fftshift(fft(coherentSC30SNR))));
+xlim([-0.5e4, 0.5e4]);
 title('Suppressed Carrier Coherent Spectrum 30 SNR');
 
 % Corrupted Coherent Detection
@@ -108,6 +124,8 @@ plot(t, corruptedSignal);
 title('Corrupted Coherent - Time Domain')
 subplot(2, 1, 2);
 plot(fs, real(fftshift(fft(corruptedSignal))));
+xlim([-0.5e4, 0.5e4]);
+ylim([-1e3, 1e3]);
 title('Corrupted Coherent - Frequency Domain');
 
 % Transmitted Carrier Modulation
@@ -118,6 +136,7 @@ modulatedTCSig = lowPassFilter(modulatedTCSig, Fc, Fs);
 figure;
 subplot(2, 1, 1);
 plot(t, modulatedTCSig);
+ylim([-0.3, 0.3]);
 title('DSB-TC Time Domain');
 subplot(2, 1, 2);
 plot(fs, real(fftshift(fft(modulatedTCSig))));
@@ -132,9 +151,11 @@ audiowrite('envelopeTC.wav', envelopeTC, Fs);
 figure;
 subplot(2, 1, 1);
 plot(t, envelopeTC);
+xlim([0, 8.5]);
+ylim([0.05, 0.3]);
 title('Transmitted Carrier Envelope');
 subplot(2, 1, 2);
-plot(Fs1, real(fftshift(fft(envelopeTC))));
+plot(fs, real(fftshift(fft(envelopeTC))));
 title('Transmitted Carrier Envelope Spectrum');
 
 
